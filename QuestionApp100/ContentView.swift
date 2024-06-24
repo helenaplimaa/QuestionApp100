d6eb1b98e36239474eafce6d6eea0ba562a2682a
Helena Lima <helena.limaa148@gmail.com>
2024-06-24 12:07:53 -0300

    Initial Commit

---
+++ QuestionApp100/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuestionApp100
+//
+//  Created by Isabel Pereira Lima on 24/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuestionApp100/QuestionApp100App.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp100App.swift
+//  QuestionApp100
+//
+//  Created by Isabel Pereira Lima on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
